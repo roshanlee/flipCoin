e4155a31ff4b3791e925caba6980c682e540cf79
roshanlee <roshan26bhagat@gmail.com>
2021-06-10 17:55:41 +0530

    The continuee till the difference of minimum 2 point

--- flipCoin.sh
+++ flipCoin.sh
@@ -2,33 +2,47 @@
 
 hedcount=0
 tailcount=0
+function achieve(){
+	while true
+	do
+		win=$((RANDOM%2))
+
+		if(($win==1))
+		then
+			hedcount=$((hedcount+1))
+			if(($hedcount==21))
+			then
+				break
+			fi
+		else
+			tailcount=$((tailcount+1))
+			if(($tailcount==21))
+			then
+				break
+			fi
+
+		fi
+	done
+}
+achieve
 while true
 do
-	win=$((RANDOM%2))
 
-	if(($win==1))
-	then
-		hedcount=$((hedcount+1))
-		if(($hedcount==21))
+	if(($hedcount>$tailcount))
 		then
-			break
-		fi
+		echo headwins by $(($hedcount-$tailcount))
+		break
+	elif(($hedcount<$tailcount))
+		then
+		echo tailwins by $(($tailcount-$hedcount))
+		break
 	else
-		tailcount=$((tailcount+1))
-		if(($tailcount==21))
+		if(($(($hedcount-$tailcount))==2 || $(($tailcount-$hedcount))==2))
 		then
+			echo $((abs($headcount-$tailcount)))
 			break
+		else
+			achieve
 		fi
-
 	fi
 done
-
-if(($hedcount>$tailcount))
-	then
-	echo headwins by $(($hedcount-$tailcount))
-elif(($hedcount<$tailcount))
-	then
-	echo tailwins by $(($tailcount-$hedcount))
-else
-	echo Tie
-fi
