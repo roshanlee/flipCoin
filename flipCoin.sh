91c0155ec4b6b485824d8a5242b140d26209346f
roshanlee <roshan26bhagat@gmail.com>
2021-06-10 14:20:41 +0530

    flipping a coin and show number

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -x
 
-win=$((RANDOM%2))
+hedcount=0
+tailcount=0
+i=0
+while((i<=10))
+do
+	win=$((RANDOM%2))
 
-if(($win==1))
-then
-	echo heads is winner
-else
-	echo Tail is winner
-fi
+	if(($win==1))
+	then
+		hedcount=$((hedcount+1))
+		echo heads is winner
+	else
+		tailcount=$((tailcount+1))
+		echo Tail is winner
+	fi
+	((i++))
+done
+echo hedcount $hedcount
+echo tailcount $tailcount
